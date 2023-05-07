6de0ad02f967e9eba15f9e47833451836cbbdef2
elavarasan-pk <elavarasan-pk@github.com>
2023-05-07 22:25:31 +0530

    Initial Commit

---
+++ src/main/java/com/hi/techpoints/RunDrools.java
@@ -0,0 +1,31 @@
+package com.hi.techpoints;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import com.hi.techpoints.model.Product;
+
+public class RunDrools {
+
+	public static final void main(String[] args) {
+		try {
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+			KieSession kSession = kContainer.newKieSession("ksession-rule");
+
+			Product product = new Product();
+			product.setModel("Pixel");
+
+		    kSession.insert(product);
+			kSession.fireAllRules();
+			
+			System.out.println("The discount for the mobile product " + product.getModel()
+			+ " is " + product.getDiscount());
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/hi/techpoints/model/Product.java
@@ -0,0 +1,24 @@
+package com.hi.techpoints.model;
+
+public class Product {
+
+	private String model;
+	private int discount;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+}
