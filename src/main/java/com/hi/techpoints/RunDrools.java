0e499300f9c7053c44a4aa0a0022f791734df408
elavarasan.kasilinga <elavarasan.kasilinga@NB-ELAVARASAN.mshome.net>
2023-05-18 14:11:41 +0530

    adding drools-kie-session

--- src/main/java/com/hi/techpoints/RunDrools.java
+++ src/main/java/com/hi/techpoints/RunDrools.java
@@ -1,49 +1,31 @@
 package com.hi.techpoints;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
-import org.drools.compiler.compiler.DroolsParserException;
-import org.drools.compiler.compiler.PackageBuilder;
-import org.drools.core.RuleBase;
-import org.drools.core.RuleBaseFactory;
-import org.drools.core.WorkingMemory;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 import com.hi.techpoints.model.Product;
 
 public class RunDrools {
 
-	public static void main(String[] args) throws DroolsParserException,
-			IOException {
-		RunDrools droolsTest = new RunDrools();
-		droolsTest.executeDrools();
-	}
+	public static final void main(String[] args) {
+		try {
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+			KieSession kSession = kContainer.newKieSession("ksession-rule");
 
-	public void executeDrools() throws DroolsParserException, IOException {
+			Product product = new Product();
+			product.setModel("Pixel");
 
-		PackageBuilder packageBuilder = new PackageBuilder();
+		    kSession.insert(product);
+			kSession.fireAllRules();
+			
+			System.out.println("The discount for the mobile product " + product.getModel()
+			+ " is " + product.getDiscount());
 
-		String ruleFile = "/com/rule/Rules.drl";
-		InputStream resourceAsStream = getClass().getResourceAsStream(ruleFile);
-
-		Reader reader = new InputStreamReader(resourceAsStream);
-		packageBuilder.addPackageFromDrl(reader);
-		org.drools.core.rule.Package rulesPackage = packageBuilder.getPackage();
-		RuleBase ruleBase = RuleBaseFactory.newRuleBase();
-		ruleBase.addPackage(rulesPackage);
-
-		WorkingMemory workingMemory = ruleBase.newStatefulSession();
-
-		Product product = new Product();
-		product.setModel("Pixel");
-
-		workingMemory.insert(product);
-		workingMemory.fireAllRules();
-
-		System.out.println("The discount for the mobile product " + product.getModel()
-				+ " is " + product.getDiscount());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
 	}
 
 }
